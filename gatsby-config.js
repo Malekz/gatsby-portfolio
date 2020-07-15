2e205033bb41a70870b833dcd376edcea8604ae9
Malek Zarzour <malekz@maleks-mbp.localdomain>
2020-07-15 14:23:58 +0200

    configured the gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -19,6 +19,17 @@ module.exports = {
       options: {},
     },
     {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    },
+    {
       resolve: "gatsby-plugin-mailchimp",
       options: {
         endpoint:
